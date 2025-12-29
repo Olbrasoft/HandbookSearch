9940a4d0f531c123d7c60cdafba93d90fa2461ac
Jirka <tuma.rsrobot@gmail.com>
2025-12-29 17:57:16 +0100

    Update DocumentImportService to support language parameter
    - Add language parameter to ImportAllAsync and ImportFileAsync
    - Filter by language when checking existing documents
    - Set Language property when creating new documents
    - Add validation for language parameter
    
    Refs #15

--- src/HandbookSearch.Business/IDocumentImportService.cs
+++ src/HandbookSearch.Business/IDocumentImportService.cs
@@ -9,15 +9,17 @@ public interface IDocumentImportService
     /// Imports all markdown files from the specified handbook directory
     /// </summary>
     /// <param name="handbookPath">Root path of the engineering handbook</param>
+    /// <param name="language">Language code (e.g., "en", "cs"). Defaults to "en".</param>
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Import result with statistics</returns>
-    Task<ImportResult> ImportAllAsync(string handbookPath, CancellationToken cancellationToken = default);
+    Task<ImportResult> ImportAllAsync(string handbookPath, string language = "en", CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Imports a single markdown file
     /// </summary>
     /// <param name="filePath">Full path to the markdown file</param>
+    /// <param name="language">Language code (e.g., "en", "cs"). Defaults to "en".</param>
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>True if the file was imported/updated, false if skipped</returns>
-    Task<bool> ImportFileAsync(string filePath, CancellationToken cancellationToken = default);
+    Task<bool> ImportFileAsync(string filePath, string language = "en", CancellationToken cancellationToken = default);
 }
